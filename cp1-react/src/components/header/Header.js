b82a13ef5a80892c0c779fe55c5ae2f3492d9fdf
ricardocossta <rm87404@fiap.com.br>
2021-08-27 18:36:28 -0300

    Estrutura Header

---
+++ cp1-react/src/components/header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+
+   return(
+      <header>
+          <ul>
+              <li>Nacional</li>
+              <li>Business</li>
+              <li>Internacional</li>
+              <li>Saúde</li>
+              <li>Tecnologia</li>
+              <li>Esporte</li>
+              <li>Entretenimento</li>
+              <li>Estilo</li>
+              <li>Viagem & Gastronomia</li>
+              <li>Newsletters</li>
+              <li>Podcasts</li>
+          </ul>
+      </header>
+)}
