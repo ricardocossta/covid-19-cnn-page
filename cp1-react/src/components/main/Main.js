95321b282f4aa98047fb7c6fecb17146f150bac1
ricardocossta <rm87404@fiap.com.br>
2021-08-27 21:03:25 -0300

    Finalizando main2

--- cp1-react/src/components/main/Main.js
+++ cp1-react/src/components/main/Main.js
@@ -7,11 +7,13 @@ export default function Main() {
 
    return(
       <div className='container-main'>
-          <h1 className='post_title'>Quais são os sinstomas da variante Delta da Covid-19? Teste seus conhecimentos</h1>
+          <h1 className='post_title'>Quais são os sintomas da variante Delta da Covid-19? Teste seus conhecimentos</h1>
+          <hr/>
           <p className='post_excerpt'>A infecção causada pela variante Delta pode provocar sintomas diferentes da doença causada pela linhagem original do novo coronavírus</p>
           <div className ='author_imagem'>
             <img src={autor} alt="Autor do artigo e estado" />
           </div>
+          <hr/>
           <div className='higher_share'>
               <span>24/08/2021 às 16:44 | Atualizado às 16:56</span>
           </div>
