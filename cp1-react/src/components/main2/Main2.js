2960f6a0e7b12d669643de775af8a442266e10c5
ricardocossta <rm87404@fiap.com.br>
2021-08-28 15:35:10 -0300

    Arrumando nome das divs

--- cp1-react/src/components/main2/Main2.js
+++ cp1-react/src/components/main2/Main2.js
@@ -9,7 +9,7 @@ export default function Main2() {
           <div className='container-logo'>
             <img className='logo' src={logo} alt="Logo da CNN Brasil" />
           </div>
-          <p>Por ser uma doença nova, a <a href="#">Covid-19</a> ainda não foi compreendida totalmente pela comunidade médica global. Os conhecimentos sobre a doença estão em constante atualização, incluindo os sintomas e os impactos para a saúde a curto e longo prazos.</p>
-          <p>A <a href="#">variante Delta da Covid-19,</a> originalmente conhecida como B.1.617.2, existe desde o final do ano passado, mas nos últimos meses tornou-se rapidamente dominante em muitos países. Ainda não está claro o quanto essa cepa é mais transmissível. Estimativas variam entre 60% e 200% mais transmissível, dependendo de quem apresenta a estimativa.</p>
+          <p className='content'>Por ser uma doença nova, a <a href="#">Covid-19</a> ainda não foi compreendida totalmente pela comunidade médica global. Os conhecimentos sobre a doença estão em constante atualização, incluindo os sintomas e os impactos para a saúde a curto e longo prazos.</p>
+          <p className='content'>A <a href="#">variante Delta da Covid-19,</a> originalmente conhecida como B.1.617.2, existe desde o final do ano passado, mas nos últimos meses tornou-se rapidamente dominante em muitos países. Ainda não está claro o quanto essa cepa é mais transmissível. Estimativas variam entre 60% e 200% mais transmissível, dependendo de quem apresenta a estimativa.</p>
       </div>
 )}
